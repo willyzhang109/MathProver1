2a24b46f787ab2aa9ec9b5b2945f847caf92c9f0
William Zhang <willyangz109@gmail.com>
2026-09-13 18:50:52 -0700

    Remove config.js typo

--- web/config.js
+++ web/config.js
@@ -1,3 +1,3 @@
 // Overwritten at deploy time by .github/workflows/pages.yml from the
 // repository variable API_BASE_URL. Edit locally for development.
-window.MATHPROVER_API_BASE = "http://mathproover-lean-wz.com/";
+window.MATHPROVER_API_BASE = "http://mathprover-lean-wz.com/";
